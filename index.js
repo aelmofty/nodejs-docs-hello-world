45ea9624870c348f4c305e869f441af89eefc543
aelmofty <126016753+aelmofty@users.noreply.github.com>
2023-02-24 19:24:57 +0200

    Update index.js

--- index.js
+++ index.js
@@ -53,16 +53,15 @@ app.options('*', cors());
 // Configure routes
 const router = express.Router();
 
+// MY Assignment World for index page
+app.get('/myassignment', function (req, res) {
+    return res.send("MyAssignment!");
+})
+
 // Hello World for index page
 app.get('/', function (req, res) {
     return res.send("Hello World!");
 })
-
-// MY Assignment World for index page
-app.get('/MyAssignment', function (req, res) {
-    return res.send("MyAssignment!");
-})
-
 
 app.get('/api', function (req, res) {
     return res.send("Fabrikam Bank API");
